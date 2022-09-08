bde5d1aa86c17188338b95e0ccbc8c649ac2af8a
Rishabh <rishabh4883@gmail.com>
2022-09-08 15:07:18 +0530

    Add DOM references and events for add and addbook button click

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function Book(title, author, pages, read) {
 function storeBook(book) {
     libraryBooks.push(book);
 }
+
+const addBook = document.querySelector('.add-book');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.close');
+const add = document.querySelector('.add-button');
+
+
+addBook.addEventListener('click', () => {
+    popup.classList.remove('hidden');
+    processForm();
+});
+
+closeButton.addEventListener('click', () => {
+    popup.classList.add('hidden');
+});
+
