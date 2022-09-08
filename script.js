813976430d37ccfb4666cbafd31bf94e154ff97e
Rishabh <rishabh4883@gmail.com>
2022-09-08 15:08:48 +0530

    Add event listeners to all input fields and read the input

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ closeButton.addEventListener('click', () => {
     popup.classList.add('hidden');
 });
 
+add.addEventListener('click', () => {
+    temp = new Book(title, author, pages, read);
+    libraryBooks.push(temp);
+});
+
+let title, author, pages, read;
+function processForm() {
+    const titleInput = document.querySelector('#title');
+    const authorInput = document.querySelector('#author');
+    const pagesInput = document.querySelector('#pages');
+    const readInput = document.querySelector('#read');
+    titleInput.addEventListener('change', (e) => {
+       title = e.target.value; 
+    });
+    authorInput.addEventListener('change', (e) => {
+        author = e.target.value;
+    });
+    pagesInput.addEventListener('change', (e) => {
+        pages = e.target.value;
+    });
+    readInput.addEventListener('change', (e) => {
+        read = e.target.value;
+    });
+}
