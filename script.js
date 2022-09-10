3a09f62fa333df3c392142bb2ffecdf21adf90db
Rishabh <rishabh4883@gmail.com>
2022-09-10 07:17:53 +0530

    Changes to the function and program design

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let libraryBooks = [];
+let count = 0;
+
+const addBook = document.querySelector('.add-book');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.close');
+const addButton = document.querySelector('.add');
+const form = document.querySelector('.form');
+
+let title = '';
+let author = '';
+let pages = '';
+let read = false;
+
+const titleInput = document.querySelector('.title');
+const authorInput = document.querySelector('.author');
+const pagesInput = document.querySelector('.pages');
+const readInput = document.querySelector('.read');
+const grid = document.querySelector('.grid');
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -11,64 +30,57 @@ function storeBook(book) {
     libraryBooks.push(book);
 }
 
-const addBook = document.querySelector('.add-book');
-const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.close');
-const add = document.querySelector('.add-button');
-
-
 addBook.addEventListener('click', () => {
+    form.reset();
     popup.classList.remove('hidden');
-    processForm();
+    
 });
 
 closeButton.addEventListener('click', () => {
     popup.classList.add('hidden');
 });
 
-add.addEventListener('click', () => {
-    temp = new Book(title, author, pages, read);
-    libraryBooks.push(temp);
+addButton.addEventListener('click', () => {
+    if(!title || !author || !pages) {return;}
+    let temp = new Book(title, author, pages, read);
+    storeBook(temp);
+
+    updateDisplay(libraryBooks[count++]);
 });
 
-let title, author, pages, read;
-function processForm() {
-    const titleInput = document.querySelector('#title');
-    const authorInput = document.querySelector('#author');
-    const pagesInput = document.querySelector('#pages');
-    const readInput = document.querySelector('#read');
-    titleInput.addEventListener('change', (e) => {
-       title = e.target.value; 
-    });
-    authorInput.addEventListener('change', (e) => {
-        author = e.target.value;
-    });
-    pagesInput.addEventListener('change', (e) => {
-        pages = e.target.value;
-    });
-    readInput.addEventListener('change', (e) => {
-        read = e.target.checked;
-    });
-}
+titleInput.addEventListener('change', (e) => {
+    title = e.target.value; 
+});
+authorInput.addEventListener('change', (e) => {
+    author = e.target.value;
+});
+pagesInput.addEventListener('change', (e) => {
+    pages = e.target.value;
+});
+readInput.addEventListener('change', (e) => {
+    read = e.target.checked;
+});
 
-function updateDisplay() {
-    const gird = document.querySelector('.grid');
-    libraryBooks.forEach((book) => {
-        gird.innerHTML = htmlTemplate(book);
-    });
+function updateDisplay(book) {
+    
+    popup.classList.add('hidden');
+    const temp = document.createElement('div');
+    temp.innerHTML = htmlTemplate(book);
+    grid.appendChild(temp);
+    
 }
 
 function htmlTemplate(book) {
     let checkbox  = 'Not read';
-    if (book[read]) {checkbox = 'Read';}
-    str = `<div class="card"> \
-    <h2>${book.title}</h2> \
-    <div>${book.author}</div> \
-    <div>${book.pages}</div> \
-    <div class="buttons"> \
-        <button class="read">${checkbox}</button> \
-        <button class="remove">remove</button> \
-    </div> \
+    if(book.read) {checkbox = 'Read';}
+    let str = `<div class="card">
+    <h2>${book.title}</h2>
+    <div>${book.author}</div>
+    <div>${book.pages}</div>
+    <div class="buttons">
+        <button class="read">${checkbox}</button>
+        <button class="remove">remove</button>
+    </div>
 </div>`
     return str;
 }
